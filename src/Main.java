3ea4ac10b8c82a92697d096c3b01d51960a1e8dc
Александр <durgen2014@gmail.com>
2022-12-11 15:54:53 +0300

    Задача 1 - 5

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
     }
 
 
@@ -33,8 +36,68 @@ public class Main {
 
         }
     }
-        public static void task3() {
-            System.out.println("Задача 3");
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Год " + year + " - высокосный");
+        } else {
+            System.out.println("Год " + year + " - НЕ высокосный");
+
+        }
 
     }
-}
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int days = 1;
+        if (deliveryDistance > 20) {
+            days++;
+        }
+        if (deliveryDistance > 60) {
+            days++;
+        }
+        if (deliveryDistance > 100) {
+            days = -1;
+        }
+        if (days != -1) {
+            System.out.println("Доставка занимает " + days + " дня");
+        } else {
+            System.out.println("Доставки нет!");
+        }
+
+
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        int mont = 11;
+        switch (mont) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Месяц под номером " + mont + " относится к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Месяц под номером " + mont + " относится к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Месяц под номером " + mont + " относится к сезону лета");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Месяц под номером " + mont + " относится к сезону осень");
+                break;
+            default:
+                System.out.println("Нет такого месяца - " + mont);
+
+        }
+    }
+    }
